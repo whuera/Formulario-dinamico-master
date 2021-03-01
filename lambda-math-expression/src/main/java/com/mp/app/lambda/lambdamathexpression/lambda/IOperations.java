c808b5e24341223bede962842935bf1234c73982
williamhuerapeya <william.huera.ext@pedidosya.com>
2021-03-01 13:27:32 -0500

    fist commit

---
+++ lambda-math-expression/src/main/java/com/mp/app/lambda/lambdamathexpression/lambda/IOperations.java
@@ -0,0 +1,10 @@
+package com.mp.app.lambda.lambdamathexpression.lambda;
+
+/**
+ * Functional Interface
+ * for lambda operations
+ */
+@FunctionalInterface
+public interface IOperations {
+    public double mathOperations(double val1, double val2);
+}
